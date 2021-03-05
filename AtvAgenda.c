7b906250590b87f9c43baa392d0f78eaa7b26e64
Pedro <ph_deoliveira@hotmail.com>
2021-03-05 12:03:25 -0300

    visualizacao temporaria

--- AtvAgenda.c
+++ AtvAgenda.c
@@ -101,7 +101,15 @@ main(){
                 }
             break;
             
-            case '2'://tesssstt
+            case '2':
+                                    // APENAS PARA TESTE
+                                    //lista
+                                    system("cls");
+                                    for(int i=0; i<maximoDePessoas; i++){
+                                        printf("\n\n %i \n %s \n %s \n %s \n %s \n %s \n\n", pessoa[i].uuid, pessoa[i].nome, pessoa[i].telefone, pessoa[i].email, pessoa[i].dataDeNascimento, pessoa[i].observ);
+                                    }
+                                    system("pause");
+                                    system("cls");
             break;
             
             case '3':
